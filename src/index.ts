c634d4ea8517cd78e6ba448d96652e3eb9885fa7
cwalls45 <cwalls45@gmail.com>
2022-11-22 23:34:39 -0500

    express server runs with 1 endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('Root endpoint');
 });
 
 app.listen(port, () => {
