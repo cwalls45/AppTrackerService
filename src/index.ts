0eac627948c1ffb6c07dbb9802591711552e6c14
cwalls45 <cwalls45@gmail.com>
2022-11-24 10:33:23 -0500

    recieve post request to create application

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import router from './router/router';
+const cors = require('cors');
 
 dotenv.config();
 
 const app: Express = express();
 const port = 3000;
+
+const corsOptions = {
+    origin: 'http://localhost:8080',
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
