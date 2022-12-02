cf9d4e6b36527ce787679ff59479319c2b849051
cwalls45 <cwalls45@gmail.com>
2022-12-01 21:43:55 -0500

    CHEM-003: get by partial chemical name

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import createApplication from '../controllers/chemicalApplication/createApplication';
 import getApplicationEvents from '../controllers/chemicalApplication/getApplicationEvents';
+import getByPartialChemicalName from '../controllers/chemicalApplication/partialChemicalName';
 
 const router = express.Router();
 
 router.get('/applicationEvents', getApplicationEvents);
+
+router.get('/partialChemicalName/:chemical', getByPartialChemicalName);
 
 router.post('/createApplication', createApplication);
 
