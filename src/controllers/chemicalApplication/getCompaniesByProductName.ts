f3915c7ed64ee465c926c476091cf8932f034c01
cwalls45 <cwalls45@gmail.com>
2022-12-08 21:42:45 -0500

    CHEM-003: finished getCompaniesByProductNAme response

--- src/controllers/chemicalApplication/getCompaniesByProductName.ts
+++ src/controllers/chemicalApplication/getCompaniesByProductName.ts
@@ -1,22 +1,22 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
-import { EPAEndpoint, IEPAPartialProductName, IProductSummary } from '../../entities/chemical';
+import { EPAEndpoint, IChemicalCompanySummary } from '../../entities/chemical';
 
 
 const getCompaniesByProductName = async (req: Request, res: Response) => {
     try {
         const productName = req.params.productName
         const response = await axios.get(`${EPAEndpoint.EPA_PRODUCT_NAME}/${productName}`);
-        const chemicalList = response.data.items as IEPAPartialProductName[];
-        // console.log(chemicalList)
-        // const activeChemicalList: IProductSummary[] =
-        //     chemicalList
-        //         .map((chemical) => ({
-        //             epaRegistrationNumber: chemical.eparegno,
-        //             productName: chemical.productname,
-        //             productStatus: chemical.product_status
-        //         }));
-        res.send({ chemicalData: chemicalList });
+        const productListSummary: IChemicalCompanySummary[] = response.data.items.map((product: any) => (
+            {
+                epaRegistrationNumber: product.eparegno,
+                productName: product.productname,
+                companyName: product.companyinfo[0].name,
+                activeIngredients: product.active_ingredients,
+                formulations: product.formulations,
+            }
+        ));
+        res.send({ chemicalData: productListSummary });
     } catch (error) {
         console.log(error);
         res.status(400).send({ error: `There was an error fetching company by product name: ${req.params.chemical}` });
