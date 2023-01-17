57286b14a07ed9ed5842db11cee381fccd622107
cwalls45 <cwalls45@gmail.com>
2023-01-16 22:18:03 -0500

    CHEM-005: add query params to getApplicationEvents

--- src/controllers/chemicalApplication/getApplicationEvents.ts
+++ src/controllers/chemicalApplication/getApplicationEvents.ts
@@ -2,10 +2,10 @@ import { Request, Response } from 'express';
 import { ApplicationEventGateway } from '../../gateways/applicationEventGateway';
 
 const getApplicationEvents = async (req: Request, res: Response) => {
-    //TODO get year and accound id from req once it is passed from the client
+    const year = Number(req.params.year);
+    const accountId = req.params.accountId;
     const applicationEventGateway = new ApplicationEventGateway();
-    const applicationEvents = await applicationEventGateway.getApplicationEventsByYear(2023, 'accountId-123');
-    //TODO: get application events once database is set up
+    const applicationEvents = await applicationEventGateway.getApplicationEventsByYear(year, accountId);
     res.locals.events = applicationEvents;
     res.send(res.locals.events);
 };
