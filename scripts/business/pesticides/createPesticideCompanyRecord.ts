6dd1672db36f4457abe4f88ff6eb5d6815588cec
cwalls45 <cwalls45@gmail.com>
2023-08-10 21:28:29 -0400

    CHEM: create company record

--- scripts/business/pesticides/createPesticideCompanyRecord.ts
+++ scripts/business/pesticides/createPesticideCompanyRecord.ts
@@ -1,16 +1,23 @@
 import fs from 'fs/promises';
-import { IPesticideInformation } from '../../../src/entities/chemical';
+import { IChemicalCompanyRecordSummary, IPesticideInformation } from '../../../src/entities/chemical';
 import axios from 'axios';
+import _ from 'lodash';
 
 async function main() {
     const unParsedPesticides = await fs.readFile('./scripts/business/pesticides/data/pesticideList.json');
     const parsedPesticideList: IPesticideInformation[] = JSON.parse(unParsedPesticides.toString());
 
-    const companyList = parsedPesticideList.map(pesticide => pesticide.companyInformation.companyName).sort();
-    const finalCompanyList = Array.from(new Set([...companyList]));
+    const companyList: IChemicalCompanyRecordSummary[] = parsedPesticideList
+        .map(pesticide => ({
+            companyName: pesticide.companyInformation.companyName,
+            companyNumber: pesticide.companyInformation.companyNumber
+        })).sort((first, second) => (first.companyName > second.companyName) ? 1 : (first.companyName < second.companyName) ? -1 : 0);
+    const finalCompanyList = _.uniqBy(companyList, 'companyName');
     await axios.post('https://9ecvyrryv5.execute-api.us-east-1.amazonaws.com/registeredPesticides/addCompanies', {
         companies: finalCompanyList
     })
+    console.log('finalCompanyList', JSON.stringify(finalCompanyList, null, 1))
+    console.log('finalCompanyList.length', finalCompanyList.length)
 }
 
 main()
