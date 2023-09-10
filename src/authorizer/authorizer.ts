13b0e4f725da7287d3be6c4d80840c479182c733
cwalls45 <cwalls45@gmail.com>
2023-09-10 19:26:14 -0400

    CHEM-032: run authorize function everytime api is invoked

--- src/authorizer/authorizer.ts
+++ src/authorizer/authorizer.ts
@@ -1,50 +1,50 @@
-import { CognitoGateway } from "../gateways/cognitoGateway";
+// import { CognitoGateway } from "../gateways/cognitoGateway";
 
-enum AllowedPaths {
+export enum AllowedPaths {
     AUTH = '/auth'
 };
 
-const allowedPaths = Object.values(AllowedPaths);
+export const allowedPaths = Object.values(AllowedPaths);
 
-const cognitoGateway = new CognitoGateway();
+// const cognitoGateway = new CognitoGateway();
 
-exports.handler = async (event: any) => {
-    console.log('event:', event)
+// exports.handler = async (event: any) => {
+//     console.log('event:', event)
 
-    const isPathWhiteListed = allowedPaths.findIndex(path => event.requestContext.http.path.includes(path)) > -1;
+//     // const isPathWhiteListed = allowedPaths.findIndex(path => event.requestContext.http.path.includes(path)) > -1;
 
-    console.log('isPathWhiteListed: ', isPathWhiteListed)
-    if (isPathWhiteListed) {
-        return generatePolicy({ allow: true });
-    }
+//     // console.log('isPathWhiteListed: ', isPathWhiteListed)
+//     // if (isPathWhiteListed) {
+//     //     return generatePolicy({ allow: true });
+//     // }
 
-    const token = event.headers &&
-        (event.headers['X-Amz-Security-Token'] || event.headers['x-amz-security-token']) ||
-        event.authorizationToken;
-    if (!token) {
-        console.log('Authorizer - No Token ID')
-        return generatePolicy({ allow: false })
-    }
+//     // const token = event.headers &&
+//     //     (event.headers['X-Amz-Security-Token'] || event.headers['x-amz-security-token']) ||
+//     //     event.authorizationToken;
+//     // if (!token) {
+//     //     console.log('Authorizer - No Token ID')
+//     //     return generatePolicy({ allow: false })
+//     // }
 
-    const isUserLoggedIn = await cognitoGateway.isUserAuthenticated(token);
-    if (!isUserLoggedIn) {
-        console.log('Authorizer - User not logged in')
-        return generatePolicy({ allow: false })
-    }
-    console.log('TokenId: ', token)
-    return generatePolicy({ allow: true })
-}
+//     // const isUserLoggedIn = await cognitoGateway.isUserAuthenticated(token);
+//     // if (!isUserLoggedIn) {
+//     //     console.log('Authorizer - User not logged in')
+//     //     return generatePolicy({ allow: false })
+//     // }
+//     // console.log('TokenId: ', token)
+//     return generatePolicy({ allow: true });
+// }
 
-function generatePolicy({ allow }: { allow: boolean }) {
-    return {
-        principalId: 'token',
-        policyDocument: {
-            Version: '2012-10-17',
-            Statement: {
-                Action: 'execute-api:Invoke',
-                Effect: allow ? 'Allow' : 'Deny',
-                Resource: '*'
-            }
-        }
-    }
-}
+// function generatePolicy({ allow }: { allow: boolean }) {
+//     return {
+//         principalId: 'token',
+//         policyDocument: {
+//             Version: '2012-10-17',
+//             Statement: {
+//                 Action: 'execute-api:Invoke',
+//                 Effect: allow ? 'Allow' : 'Deny',
+//                 Resource: '*'
+//             }
+//         }
+//     }
+// }
---
+++ src/controllers/auth/authorize.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import { CognitoGateway } from '../../gateways/cognitoGateway';
+import { allowedPaths } from '../../authorizer/authorizer';
+
+const authorize = async (req: Request, res: Response, next: NextFunction) => {
+    const cognitoGateway = new CognitoGateway();
+    try {
+
+        const isPathWhiteListed = allowedPaths.findIndex(path => req.url.includes(path)) > -1;
+        console.log('isPathWhiteListed:', isPathWhiteListed)
+
+        if (isPathWhiteListed) {
+            return next();
+        }
+
+        const token = req.headers &&
+            (req.headers['X-Amz-Security-Token'] || req.headers['x-amz-security-token']) as string;
+
+        if (!token) {
+            console.log('Authorizer - No Token ID')
+            res.status(403).end()
+        }
+
+        const isUserLoggedIn = await cognitoGateway.isUserAuthenticated(token);
+        if (!isUserLoggedIn) {
+            console.log('Authorizer - User not logged in')
+            res.status(403).end()
+        }
+
+        next();
+
+    } catch (error) {
+
+    }
+};
+
+export default authorize;
